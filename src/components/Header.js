5e66970a073b4600c9931b5ff720fe5c2b3269ce
jstorer <jeffrey.storer@gmail.com>
2021-01-11 16:30:29 -0500

    1/11 Saturday Madness

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
       <div className="header">TLC Golf</div>
       <br></br>
       <div className="center">
-        <h3>Saturday Group</h3>
+        <h3>Saturday Madness</h3>
         <h4>Today's Handicaps</h4>
       </div>
     </>
